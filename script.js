a2a4e26d8db44679a782ad563051153110b6142e
Yuri Ramos <yroramos@gmail.com>
2022-10-30 21:43:06 -0300

    Começando no JS

---
+++ script.js
@@ -0,0 +1,6 @@
+var nome = "Yuri"
+var idade = prompt("digite sua idade");
+// document estamos nos referindo ao conteudo da janela 
+// Window estamos nos referindo a janela
+
+document.write("<img src='http://www.google.com/google.jpg'/>")
