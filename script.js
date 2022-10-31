21506b0f5ec98d6142fae6bac19fe119c2db1fe0
Yuri Ramos <yroramos@gmail.com>
2022-10-30 22:46:15 -0300

    Começando no JS Parte 2

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-var nome = "Yuri"
-var idade = prompt("digite sua idade");
+// var nome = "Yuri"
+// var idade = prompt("digite sua idade");
 // document estamos nos referindo ao conteudo da janela 
 // Window estamos nos referindo a janela
 
-document.write("<img src='http://www.google.com/google.jpg'/>")
+document.write("<img src='http://www.google.com/google.jpg'/>");
+
+function entrar() {
+    var area = document.getElementById ('area');
+    var texto = prompt('Digite um texto');
+    if (texto === ''){
+        alert('digite seu nome');
+        area.innerHTML ='bem vindo ..';
+    }
+    
+    area.innerHTML ='bem vindo  '+ texto;
+}
+
+function entrar2(nome){
+    var area = document.getElementById ('area2');
+    var texto = prompt('Digite sobrenome');
+   
+    area.innerHTML = nome+ "  " + texto;
+}
