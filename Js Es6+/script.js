20de5c60a4d05dcc6d1d350df3e6ae507c2adbe3
Yuri Ramos <yroramos@gmail.com>
2022-10-30 23:46:35 -0300

    Loops

---
+++ Js Es6+/script.js
@@ -0,0 +1,18 @@
+// while = enquanto
+ var x=0;
+while(x<10){
+
+    document.write("</br> ovalor do x é :"+ x);
+    console.log(x);
+
+    x++;
+}
+
+document.write("</br></br></br></br></br> ovalor do x é :"+x);
+
+// for = para
+for (a=0; a<x; a++){
+
+    document.write("</br> ovalor do y é :"+ a);
+    console.log(x);
+}
