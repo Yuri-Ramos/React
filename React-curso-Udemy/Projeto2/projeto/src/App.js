06386a4ba5ea10921a56bd3fd4d867b50c5d9e82
Yuri Ramos <yroramos@gmail.com>
2022-12-07 16:24:59 -0300

    Iniciando novo curso da Udemy sobre aprofundamentos no React

---
+++ React-curso-Udemy/Projeto2/projeto/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import logo from './logo.svg';
+import Yuri from './Yuri';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Meu Projeto em React
+        </p>
+      <Yuri/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React-curso-Udemy/Projeto2/projeto/src/Yuri.js
@@ -0,0 +1,18 @@
+
+
+
+function Yuri() {
+  return (
+    <div className="App">
+  
+        
+        <p>
+         Eu sou um component chamado Yuri
+        </p>
+      
+      
+    </div>
+  );
+}
+
+export default Yuri;
